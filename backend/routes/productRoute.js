4d60fadc9d2ae54e838dc83777e02460378cb76b
Amit <amitshuu@gmail.com>
2022-04-19 12:41:23 +0300

    Product api routes

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {
+  getProducts,
+  getSingleProduct,
+} from '../controllers/productController.js';
+
+const router = express.Router();
+
+router.route('/').get(getProducts);
+router.route('/:id').get(getSingleProduct);
+
+export default router;
