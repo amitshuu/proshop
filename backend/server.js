757cedcd7833270454e44f1bf9b88727935f43ee
Amit <amitshuu@gmail.com>
2022-04-19 11:39:20 +0300

    Implemented middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
+import 'express-async-errors';
+
 import express from 'express';
-import products from './data/products.js';
 import 'dotenv/config';
 import connectDB from './db/connect.js';
 
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+import productRoute from './routes/productRoute.js';
+import notFoundMiddleware from './middleware/not-found.js';
+
 const app = express();
 
+app.use('/api/products', productRoute);
 app.get('/', (req, res) => {
   res.send('Api is runing ....');
 });
 
-app.get('/api/products', (req, res) => {
-  res.send(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.send(product);
-});
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 4000;
 
