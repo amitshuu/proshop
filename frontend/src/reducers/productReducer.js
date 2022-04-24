44649fc6006b2fe253d59f77f8c65d1ac847e163
Amit <amitshuu@gmail.com>
2022-04-24 13:45:14 +0300

    Single product reducer

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -2,17 +2,36 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAILED,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAILED,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { isLoading: true, products: [] };
+      return { ...state, isLoading: true };
     case PRODUCT_LIST_SUCCESS:
       return { isLoading: false, products: action.payload };
     case PRODUCT_LIST_FAILED:
       return { isLoading: false, error: action.payload };
 
+    default:
+      return state;
+  }
+};
+
+export const productDeatilsReducer = (
+  state = { singleProduct: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { isLoading: true };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { isLoading: false, singleProduct: action.payload };
+    case PRODUCT_DETAILS_FAILED:
+      return { isLoading: false, error: action.payload };
     default:
       return state;
   }
