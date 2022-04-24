e0b59f7d9354a8473125219f9a97f4d6f582a8b7
Amit <amitshuu@gmail.com>
2022-04-24 13:47:58 +0300

    Added castError

--- backend/middleware/error-handler.js
+++ backend/middleware/error-handler.js
@@ -1,8 +1,13 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
   const defaultError = {
-    statusCode: err.statusCode || 500,
-    msg: err.message || 'Something went wrong!',
+    statusCode: res.statusCode === 200 ? 500 : res.statusCode,
+    msg: err.msg || 'Something went wrong',
   };
+
+  if (err.name === 'CastError' || res.statusCode === 404) {
+    defaultError.statusCode === 404;
+    defaultError.msg = 'Product not found!';
+  }
 
   res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
