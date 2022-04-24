ea008f317863072ce62bf0ffa4e69919a1dd7ec8
Amit <amitshuu@gmail.com>
2022-04-24 13:48:09 +0300

    Implemented cart

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,27 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducer';
+//Product reducer
+import {
+  productDeatilsReducer,
+  productListReducer,
+} from './reducers/productReducer';
 
+//Cart reducer
+
+import { cartReducer } from './reducers/cartReducer';
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDeatilsReducer,
+  cart: cartReducer,
 });
-const initialState = {};
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const store = createStore(
   reducer,
