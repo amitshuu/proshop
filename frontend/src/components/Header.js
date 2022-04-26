9f3ca715db256066bbe2d8e690fcf1c7b3ef41f5
Amit <amitshuu@gmail.com>
2022-04-26 11:56:39 +0300

    Implemented user to the nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
+import { logout } from '../actions/userActions';
+import { useNavigate } from 'react-router-dom';
 const Header = () => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    navigate('/login');
+  };
   return (
     <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
       <Container>
@@ -17,12 +29,23 @@ const Header = () => {
                 Cart
               </Nav.Link>
             </LinkContainer>
-            <LinkContainer to='/login'>
-              <Nav.Link>
-                <i className='fas fa-user' />
-                Sign In
-              </Nav.Link>
-            </LinkContainer>
+
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id='username'>
+                <LinkContainer to='/profile'>
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            ) : (
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
