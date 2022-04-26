6aa86e8911df49e4cee2c4aa1dfff4adc0b6e63f
Amit <amitshuu@gmail.com>
2022-04-26 11:57:14 +0300

    User info added to the redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+
 //Product reducer
 import {
   productDeatilsReducer,
@@ -8,19 +9,28 @@ import {
 } from './reducers/productReducer';
 
 //Cart reducer
-
 import { cartReducer } from './reducers/cartReducer';
+
+//User reducer
+import { userSetupReducer } from './reducers/userReducer';
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDeatilsReducer,
   cart: cartReducer,
+  userLogin: userSetupReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const store = createStore(
