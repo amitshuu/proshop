59d3203876f5a9da9a6001f2c08be81c9a58512e
Amit <amitshuu@gmail.com>
2022-04-26 11:57:41 +0300

    References

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,15 +29,19 @@ const userSchema = mongoose.Schema(
   }
 );
 
+//Hash password
 userSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
   const salt = await bcryptjs.genSalt(10);
   this.password = await bcryptjs.hash(this.password, salt);
 });
+
+//Compare passwords
 userSchema.methods.comparePasswords = async function (enteredPassword) {
   return await bcryptjs.compare(enteredPassword, this.password);
 };
 
+//Create JsonWebToken
 userSchema.methods.createJWT = function () {
   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
