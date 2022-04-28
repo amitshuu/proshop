ca304de9c7c3cf1328ee160bf50eed1653e8de2e
Amit <amitshuu@gmail.com>
2022-04-28 16:00:34 +0300

    Define min length to password field

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,6 +17,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, 'Please provide a passowrd'],
       select: false,
+      minlength: 6,
     },
     isAdmin: {
       type: Boolean,
