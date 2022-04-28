cf706eb042191dea3e7dad05fdbe85fe4d1be628
Amit <amitshuu@gmail.com>
2022-04-28 12:22:12 +0300

    Update user profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,12 @@ import {
   USER_SETUP_REQUEST,
   USER_SETUP_SUCCESS,
   USER_SETUP_FAILED,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAILED,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAILED,
 } from '../constants/userConstants';
 import axios from 'axios';
 export const setupUser =
@@ -18,6 +24,51 @@ export const setupUser =
       dispatch({ type: USER_SETUP_FAILED, payload: error.response.data.msg });
     }
   };
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_DETAILS_REQUEST });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/users/${id}`, config);
+
+    dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: USER_DETAILS_FAILED, payload: error.response.data.msg });
+  }
+};
+
+export const updateUserProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_UPDATE_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.put('/api/users/profile', user, config);
+
+    dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
+    dispatch({ type: USER_SETUP_SUCCESS, payload: data });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({ type: USER_UPDATE_FAILED, payload: error.response.data.msg });
+  }
+};
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
