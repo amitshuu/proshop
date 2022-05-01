27ffeb4e13bccf36713a3954b2f267a07f87f115
Amit <amitshuu@gmail.com>
2022-05-01 14:19:42 +0300

    Exports\Imports

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,6 +5,7 @@ import Rating from './Rating';
 import Loading from './Loading';
 import AlertMessage from './AlertMessage';
 import FormContainer from './FormContainer';
+import CheckoutSteps from './CheckoutSteps';
 
 export {
   Header,
@@ -13,5 +14,6 @@ export {
   Rating,
   Loading,
   AlertMessage,
+  CheckoutSteps,
   FormContainer,
 };
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,5 +2,18 @@ import HomeScreen from './HomeScreen';
 import ProductScreen from './ProductScreen';
 import CartScreen from './CartScreen';
 import LoginScreen from './LoginScreen';
+import ProtectedRoute from './ProtectedRoute';
+import ShippingScreen from './ShippingScreen';
+import PaymentScreen from './PaymentScreen';
+import Placeorder from './Placeorder';
 
-export { HomeScreen, ProductScreen, CartScreen, LoginScreen };
+export {
+  HomeScreen,
+  ProductScreen,
+  CartScreen,
+  LoginScreen,
+  ProtectedRoute,
+  ShippingScreen,
+  PaymentScreen,
+  Placeorder,
+};
